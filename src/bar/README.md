a14caac7c2b91a305a7f5ef779bf3385a0b45f50
Louis Maddox <louismmx@gmail.com>
2024-01-14 15:20:42 +0000

    docs: delete unnecessary extra parts (easier to inspect debug output with fewer docstrings processed)

--- src/bar/README.md
+++
@@ -1,7 +0,0 @@
----
-icon: fontawesome/solid/toggle-off
----
-
-# Bar
-
-This is an example of a service which does very little for now.
--- src/bar/src/bar/main.py
+++
@@ -1,9 +0,0 @@
-__all__ = ["example_function"]
-
-
-def example_function(a: int, b: str = "hello this is bar:main.b") -> None:
-    """
-    This is the example function provided by the bar package.
-    """
-    print(a, b)
-    return
