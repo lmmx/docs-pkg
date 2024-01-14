26ddfd2131b07cbd073376724819e5725085115b
Louis Maddox <louismmx@gmail.com>
2024-01-14 15:26:43 +0000

    docs: reduce to 1 link

--- src/foo/src/foo/main.py
+++ src/foo/src/foo/main.py
@@ -8,7 +8,7 @@ class A(BaseModel):
     """
     This is an example of a documented Pydantic model.
 
-    [foo.main.A.x][] should give the same link as [.x][foo.main.A.x]
+    [.x][foo.main.A.x]
 
     Attributes:
         x: An example number.
